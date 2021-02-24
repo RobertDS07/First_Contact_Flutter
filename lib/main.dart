5d8cf3df9dcdf8e37410393fec94be6b356ad985
robertds07 <robertdamacenobob@gmail.com>
2021-02-24 13:17:17 -0300

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(
-      title: 'A simple calculator',
+      title: 'Currency converter',
+      theme: ThemeData(primaryColor: Colors.white, hintColor: Colors.amber),
       home: Home(),
     ));
 
@@ -9,115 +13,39 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('A simple calculator'),
+        title: Text('Currency converter'),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.amber,
       ),
-      body: Calculator(),
-    );
-  }
-}
-
-class Calculator extends StatefulWidget {
-  @override
-  _CalculatorState createState() => _CalculatorState();
-}
-
-class _CalculatorState extends State<Calculator> {
-  dynamic result = 0;
-
-  // criar uma var para pegar os valores digitados apenas para mostrar a cima do resultado para o user enquanto isso anoto os numeros digitados em uma list e as operações digitadas em outro
-
-  static List<Map> skeleton = [
-    {'hotkey': 'C', 'action': () => null},
-    {'hotkey': Icons.arrow_back_ios_outlined, 'action': () => null},
-    {'hotkey': '%', 'action': () => null},
-    {'hotkey': 'X', 'action': () => null},
-    {'hotkey': '-', 'action': () => null},
-    {'hotkey': '+', 'action': () => null},
-    ...List.generate(
-        3, (index) => ({'hotkey': '${index + 1}', 'action': () => null})),
-    ...List.generate(
-        3, (index) => ({'hotkey': '${index + 4}', 'action': () => null})),
-    ...List.generate(
-        3, (index) => ({'hotkey': '${index + 7}', 'action': () => null})),
-    {'hotkey': '.', 'action': () => null},
-    {'hotkey': '0', 'action': () => null},
-    {'hotkey': '=', 'action': () => null},
-  ];
-
-  final List<Widget> hotkeys =
-      skeleton.map((e) => CustomButton(e['hotkey'])).toList();
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Container(
-          color: Colors.black,
-          height: 100,
-          child: Align(
-            alignment: Alignment.bottomRight,
-            child: Text(
-              '$result',
-              textAlign: TextAlign.right,
-              style: TextStyle(
-                  fontSize: 90,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(
+            Icons.monetization_on,
+            size: 120,
+            color: Colors.amber,
           ),
-        ),
-        Divider(
-          color: Colors.grey,
-          height: 1,
-        ),
-        Container(
-          color: Colors.black,
-          child: GridView.count(
-            padding: const EdgeInsets.all(10),
-            crossAxisCount: 3,
-            childAspectRatio: MediaQuery.of(context).size.height / 568,
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            children: hotkeys,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final hotkey;
-
-  CustomButton(this.hotkey);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(40),
-        splashColor: Colors.grey.withAlpha(40),
-        onTap: () {
-          print('Card tapped.');
-        },
-        child: Center(
-          child: hotkey is String
-              ? Text(
-                  '$hotkey',
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                )
-              : Icon(
-                  hotkey,
-                  size: 40,
-                  color: Colors.white,
-                ),
-        ),
+          Convertor()
+        ],
       ),
     );
   }
+}
+
+class Convertor extends StatefulWidget {
+  @override
+  _ConvertorState createState() => _ConvertorState()
+}
+
+class _ConvertorState extends State<Convertor> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(builder: null)
+  }
+}
+
+Future<map> getData() {
+  http.get()
 }
